b5a800d161c256e6f8f3a357ad8a02d25d36c88a
Chen Zhu <cz764@nyu.edu>
2025-12-19 11:51:49 -0500

    feat: add search bar filter and category select

---
+++ app/components/SearchAndFilterBar/CategorySelect.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Select, SelectSection, SelectItem } from '@heroui/select';
+
+export const animals = [
+  { key: 'cat', label: 'Cat' },
+  { key: 'dog', label: 'Dog' },
+  { key: 'elephant', label: 'Elephant' },
+  { key: 'lion', label: 'Lion' },
+  { key: 'tiger', label: 'Tiger' },
+  { key: 'giraffe', label: 'Giraffe' },
+  { key: 'dolphin', label: 'Dolphin' },
+  { key: 'penguin', label: 'Penguin' },
+  { key: 'zebra', label: 'Zebra' },
+  { key: 'shark', label: 'Shark' },
+  { key: 'whale', label: 'Whale' },
+  { key: 'otter', label: 'Otter' },
+  { key: 'crocodile', label: 'Crocodile' },
+];
+
+export default function CategorySelect() {
+  return (
+    <Select
+      className='max-w-xs'
+      area-label='Category'
+      placeholder='Select a category'
+    >
+      {animals.map((animal) => (
+        <SelectItem key={animal.key}>{animal.label}</SelectItem>
+      ))}
+    </Select>
+  );
+}
--- app/components/SearchAndFilterBar/index.tsx
+++ app/components/SearchAndFilterBar/index.tsx
@@ -1,15 +1,43 @@
 import { Button } from '@heroui/button';
 import { Link } from '@heroui/link';
+import { Input } from '@heroui/input';
+import CategorySelect from './CategorySelect';
+import SearchIcon from '@/components/Icons/SearchIcon';
+import FilterIcon from '@/components/Icons/FilterIcon';
 
 export default function SearchAndFilterBar() {
   return (
-    <div className='flex gap-6'>
-      <div>Search</div>
-      <div>filter icon</div>
-      <div>Category select</div>
-      <Button as={Link} color='primary' href='/add'>
-        Add
-      </Button>
+    <div className='flex flex-col gap-1'>
+      <div className='mt-4 flex justify-between'>
+        <div className='flex w-xl items-center gap-6'>
+          <Input
+            className='w-sm'
+            label='Search'
+            size='sm'
+            endContent={<SearchIcon className='pointer-events-none size-6' />}
+          />
+
+          <Button
+            isIconOnly
+            aria-label='Filter'
+            color='primary'
+            variant='faded'
+          >
+            <FilterIcon className='size-6' />
+          </Button>
+          <CategorySelect />
+        </div>
+        <Button as={Link} color='primary' href='/add'>
+          Add
+        </Button>
+      </div>
+      <div className='flex justify-between'>
+        <div className='flex gap-4'>
+          <p>Filter item1</p>
+          <p>Filter item2</p>
+        </div>
+        <p className='text-gray-700'>20 recipes</p>
+      </div>
     </div>
   );
 }
