2f7759d44864f363fb86e7275421a6493aafbb5d
Chen Zhu <cz764@nyu.edu>
2025-12-23 10:06:45 -0500

    chore: add footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,7 @@
 export default function Footer() {
   return (
-    <footer className='bg-nav-background'>footer should be on bottom</footer>
+    <footer className='bg-nav-background p-4'>
+      <p>@Chen Zhu. All rights reserved.</p>
+    </footer>
   );
 }
